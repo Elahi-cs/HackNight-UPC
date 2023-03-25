2b2e195248a37231c18412ba268af71f81995480
Ivan <ivbuirez@gmail.com>
2023-03-25 03:37:14 +0100

    player con limites

--- Player.py
+++ Player.py
@@ -18,18 +18,23 @@ class Player(Rect):
         self.moving[0] = self.keys[pygame.K_a]
         self.moving[1] = self.keys[pygame.K_d]
         
-        if self.keys[pygame.K_w]:
+        if self.keys[pygame.K_w] and self.y == 720 - 80:
             self.vel[1] = -10
         
         #if self.keys[pygame.K_s]:
         #    self.bottomright[1] += self.vel[1]
             
-        if self.keys[pygame.K_a]:
+        if self.keys[pygame.K_a] and self.x >= 0:
             self.x -= self.vel[0]
-        if self.keys[pygame.K_d]:
+        if self.keys[pygame.K_d] and self.x < 1280-50:
             self.x += self.vel[0]
-    
-        self.y += self.vel[1]
+
+        if(self.y + self.vel[1] < 720 - 80):
+            self.y += self.vel[1]
+        else:
+            self.y = 720 - 80
+            
+
 
     def blitme(self):
         pygame.draw.rect(self.screen, (0, 0, 255), self)
